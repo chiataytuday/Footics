4c7e0e4bdc64c15074c86aa2cb961ad604f0be08
Kim Ju Hoon <galaxy1014@naver.com>
2020-01-08 17:45:26 +0900

    인스턴스 정리
    각 리그별 클럽들 배열로 정리

--- Footics/Footics/ViewController.swift
+++ Footics/Footics/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     let serie = ["Juventus", "Internazionale", "Lazio", "Roma", "Atlanta", "Bologna", "Brescia", "Cagliari", "Florentina", "Geona", "Hellas Verona", "Lecce", "Milan", "Napoli", "Parma", "Sampdoria", "Sassuolo", "SPAL", "Torino", "Udinese"]
     let league1 = ["Paris Saint-Germain", "Olympique de Marseille", "Stade Rennais FC", "LOSC", "FC Nantes", "Stade de Reims", "AS Monaco", "Angers SCO", "Montpellier Herault SC", "OGC Nice", "RC Strasbourg Alsace", "Olympique Lyonnais", "Girondins de Bordeaux", "AS Saint-Etienne", "Stade Brestois 29", "Dijon FCO", "FC Metz", "Amiens SC", "Nimes Olympique", "Toulouse FC"]
     let bundes = ["FC Bayern Munchen", "Borussia Dortmund", "RB Leipzig", "Bayer 04 Leverkusen", "Borussia Monchengladbach", "VfL Wolfsburg", "Eintracht Frankfurt", "SV Werder Bremen", "TSG 1899 Hoffenheim", "Fortuna Düsseldorf", "Hertha Berlin", "FSV Mainz 05", "Sport-Club Freiburg", "FC Schalke 04", "FC Augsburg", "FC Koln", "SC Paderborn 07", "FC Union Berlin"]
-    let laliga = ["Real Madrid", ...]
+    let laliga = ["Real Madrid","Athletic Club","Atletico de Madrid","C.A. Osasuna","CD Leganes", "D. Alaves", "FC Barcelona","Getafe CF", "Granada CF" ,"Levante UD","RC Celta","RCD Espanyol de Barcelona" ,"RCD Mallorca","Real Betis","Real Madrid", "Real Sociedad","Real Valladolid CF","SD Eibar","Sevilla FC","Valencia CF","Villarreal CF"]
     
     
     override func viewDidLoad() {
