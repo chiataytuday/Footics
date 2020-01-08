25873be1585f4a58da412a786d4906f878319a2e
Kim Ju Hoon <galaxy1014@naver.com>
2020-01-08 17:37:12 +0900

    인스턴스 종합
    리그 인스턴스

--- Footics/Footics/ViewController.swift
+++ Footics/Footics/ViewController.swift
@@ -20,9 +20,9 @@ class ViewController: UIViewController {
         let Europa = "Europa League"
     }
     
-    let premier = ["Manchester United", ...]
-    let serie = ["Juventus", ...]
-    let league1 = ["Paris Saint-German",...]
+    let premier = ["Manchester United", "Arsenal", "Aston Villa", "AFC Bournemouth", "Brighton and Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Leicester City", "Liverpool", "Manchester City", "Newcastle United", "Norwich City", "Sheffield United", "Southampton", "Tottenham Hotspur", "Watford", "West Ham United", "Wolverhampton Wanderers"]
+    let serie = ["Juventus", "Internazionale", "Lazio", "Roma", "Atlanta", "Bologna", "Brescia", "Cagliari", "Florentina", "Geona", "Hellas Verona", "Lecce", "Milan", "Napoli", "Parma", "Sampdoria", "Sassuolo", "SPAL", "Torino", "Udinese"]
+    let league1 = ["Paris Saint-Germain", "Olympique de Marseille", "Stade Rennais FC", "LOSC", "FC Nantes", "Stade de Reims", "AS Monaco", "Angers SCO", "Montpellier Herault SC", "OGC Nice", "RC Strasbourg Alsace", "Olympique Lyonnais", "Girondins de Bordeaux", "AS Saint-Etienne", "Stade Brestois 29", "Dijon FCO", "FC Metz", "Amiens SC", "Nimes Olympique", "Toulouse FC"]
     let bundes = ["Byern Munchen", ...]
     let laliga = ["Real Madrid", ...]
     
