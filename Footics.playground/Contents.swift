94623bbf56a64450fb4caa9bf9139a76e0a97c86
Kim Ju Hoon <galaxy1014@naver.com>
2020-01-08 15:19:44 +0900

    문제
    플레이그라운드 업로드

---
+++ Footics.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+//
+//var str = "Hello, playground"
+//
+//// 문제1
+//
+//var quanxs = ["Denma", "Lot", "Gongja", "Jiro", "Marbin"]
+//for quanx in quanxs {
+//    if quanx.count > 3 {
+//        print(quanx)
+//    }
+//    if quanx.hasPrefix("D"){
+//        print(quanx)
+//    }
+//}
+//
+//// 풀이
+//
+//quanxs.filter(){$0.count > 3}.map(){print($0)}
+
+// 문제2
+
+var fruits = ["Banana", "Apple", "Watermelon", "Grape", "Peach"]
+
+for fruit in fruits{
+    if fruit.count > 4 && fruit.hasSuffix("e")
+    {
+        print(fruit)
+    }
+}
+
+// 문제 3
+
+var score = [100, 87, 65, 94, 88, 41, 52]
+
+var max = 0
+
+for i in score{
+    if i > max
+    {
+        max = i
+        print(max)
+    }
+}
+
+// 문제 4
+
+var score2 = [37, 87, 65, 94, 88, 41, 52]
+
+var min = 9999
+
+for i in score2{
+    if i < min
+    {
+        min = i
+        print(min)
+    }
+}
+
+// 문제 5
+
+var iphones = ["iPhone 3GS", "iPhone X", "iPhone 8", "iPhone 11 Pro", "iPhone XR"]
+
+for phone in iphones{
+    if phone.count > 8{
+        print(phone)
+    }
+}
+
+// 문제 6
+
+var pixels = ["Pixel 3A XL", "Pixel 3A", "Pixel 3", "Pixel 3 XL", "Pixel 4"]
+
+for pixel in pixels{
+    if pixel.count > 7{
+        print(pixel)
+    }
+}
