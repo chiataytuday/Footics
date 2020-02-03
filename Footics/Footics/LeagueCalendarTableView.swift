44f06cf11d808cd5d5607295280472d8a5c04434
Kim Ju Hoon <galaxy1014@naver.com>
2020-02-03 12:31:25 +0900

    모델 보완 및 초기화면 조정
    모델에 들어가는 데이터 추가 및 초기화면 조정

--- Footics/Footics/LeagueCalendarTableView.swift
+++ Footics/Footics/LeagueCalendarTableView.swift
@@ -27,6 +27,19 @@ class LeagueCalendarTableView: UIViewController {
     var sections: [String] = ["England-Premier League", "Spain-Laliga", "Italy-SerieA", "Germany-Bundesliga", "France-League1"]
 
     
+    let mlModel = Classification()
+    let homeScoreModel = HomeScore()
+    let awayScoreModel = HomeScore_3()
+    var temp = [String]()
+    var home = ""
+    var away = ""
+    var draw = ""
+    var homeScore = 0
+    var awayScore = 0
+    var homeClub = ""
+    var awayClub = ""
+    var resultDict = [String: Double]()
+    
     @IBOutlet weak var premierTable: UITableView!
     
     override func viewDidLoad() {
@@ -42,29 +55,51 @@ class LeagueCalendarTableView: UIViewController {
 
               }catch{
                   print(error)
-              }                
-      
+              }
+            for i in schedules{
+                homeClub = i.hometeam
+                awayClub = i.awayteam
+                let prediction = try? mlModel.prediction(input: ClassificationInput(home_team: homeClub, away_team: awayClub))
+                let homePrediction = try? homeScoreModel.prediction(input: HomeScoreInput(home_team: homeClub, away_team: awayClub))
+                let awayPrediction =   try? awayScoreModel.prediction(input: HomeScore_3Input(home_team: homeClub, away_team: awayClub))
+                
+                homeScore = Int(round(homePrediction!.home_goals))
+                awayScore = Int(round(awayPrediction!.away_goals))
+
+                resultDict = prediction!.resultProbability
+                print(homeScore, awayScore)
+                for (key,value) in resultDict{
+                    if(key == "H"){
+                        home = "홈 승리: " + Int(value * 100).description + "% " // 팀1 승리 확률
+                    }else if(key == "A"){
+                        away = "어웨이 승리: " + Int(value * 100).description + "% " // 무승부 확률
+                    }else{
+                        draw = "무승부: " + Int(value * 100).description + "% " // 팀2 승리 확률
+                    }
+                }
+//        temp = home + away + draw
+        temp.append("예측 결과 : \(homeScore) vs \(awayScore)")
+//        print(temp)
+        }
+        
          premierTable.delegate = self
          premierTable.dataSource = self
-         
     }
-    
-    
 }
-
 
 extension LeagueCalendarTableView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.schedules.count
         return self.schedules.count
     }
     
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        return Array(Set(self.sections.map{$0.first! })).count
-    }
-    
-    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
-    }
+//    public func numberOfSections(in tableView: UITableView) -> Int {
+//        return Array(Set(self.sections.map{$0.first! })).count
+//    }
+//
+//    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return sections[section]
+//    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleList", for: indexPath) as! LeagueCalandarCell
@@ -73,6 +108,7 @@ extension LeagueCalendarTableView: UITableViewDataSource, UITableViewDelegate {
         cell.homeTeamImage.image = UIImage(named: scheduleRowData.hometeam)
         cell.awayTeamImage.image = UIImage(named: scheduleRowData.awayteam)
         cell.timeLabel.text = scheduleRowData.time
+        cell.resultLabel.text = temp[indexPath.row]
         return cell
     }
     
--- Footics/Footics/ResultPredictViewController.swift
+++ Footics/Footics/ResultPredictViewController.swift
@@ -12,49 +12,123 @@ import CoreML
 class ResultPredictViewController: UIViewController{
     var teamOne = ""
     var teamTwo = ""
-    @IBOutlet weak var teamOneName: UILabel!
     
-    @IBOutlet weak var teamTwoName: UILabel!
+    var homewin: Float = 0.0 // 홈 팀이 이길 확률
+    var draw: Float = 0.0 // 비길 확률
+    var awaywin: Float = 0.0 // 홈 팀이 이길 확률
     
-    @IBOutlet weak var teamOneLogo: UIImageView!
+    var teamOneGoal = 0 // 팀 1 득점
+    var teamTwoGoal = 0 // 팀 2 득점
     
-    @IBOutlet weak var teamTwoLogo: UIImageView!
+     @IBOutlet weak var teamOneName: UILabel!
+       @IBOutlet weak var teamOneScore: UILabel!
+       @IBOutlet weak var teamOneWinRate: UILabel!
+       
+       @IBOutlet weak var DrawRate: UILabel!
+       
+       @IBOutlet weak var teamTwoName: UILabel!
+       @IBOutlet weak var teamTwoScore: UILabel!
+       
+       @IBOutlet weak var teamOneLogo: UIImageView!
+       
+       @IBOutlet weak var teamTwoLogo: UIImageView!
+       @IBOutlet weak var teamTwoWinRate: UILabel!
+       
+       @IBOutlet weak var VictoryRateBar1: UIProgressView!
+       @IBOutlet weak var VictoryRateBar2: UIProgressView!
+       @IBOutlet weak var VictoryRateBar3: UIProgressView!
+       @IBOutlet weak var VictoryRateBarStack: UIStackView!
+       
+       @IBOutlet weak var recentMatchTableView: UITableView!
+       
+       @IBOutlet weak var VictoryRateBar1_constr: NSLayoutConstraint!
+       @IBOutlet weak var VictoryRateBar2_constr: NSLayoutConstraint!
+       @IBOutlet weak var VictoryRateBar3_constr: NSLayoutConstraint!
+       
+       @IBOutlet weak var VictoryRateBar_trailing_constr: NSLayoutConstraint!
+       @IBOutlet weak var VictoryRateBar_leading_constr: NSLayoutConstraint!
+       
     
-    
-    let mlModel = MyTabularClassifier_1()
-    let homeScoreModel = HomeScore_1()
+    let mlModel = Classification()
+    let homeScoreModel = HomeScore()
+    let awayScoreModel = HomeScore_3()
     var temp = 0
     var homeScore = 0
+    var awayScore = 0
     var resultDict = [String: Double]()
 
     
     override func viewDidLoad() {
             super.viewDidLoad()
-        teamOneName.text = teamOne
-        teamTwoName.text = teamTwo
-        teamOneLogo.image = UIImage(named: teamOne)
-        teamTwoLogo.image = UIImage(named: teamTwo)
+               teamOneName.text = teamOne
+         teamTwoName.text = teamTwo
+         
+         teamOneLogo.image = UIImage(named: teamOne)
+         teamTwoLogo.image = UIImage(named: teamTwo)
+         
+         // 아래에 학습된 모델에서 나온 결과를 반영하면 됨
+         // 예상 득점
+         teamOneGoal = 2 // 팀 1 득점
+         teamTwoGoal = 3 // 팀 2 득점
+         
+         // 예상 승률 (합계 1.0)
+         homewin = 0.45 // 팀 1 승리 확률
+         draw = 0.10 // 무승부 확률
+         awaywin = 0.45 // 팀 2 승리 확률
+         // 여기 까지!
+         
+         // 예상 득점 설정
+         teamOneScore.text = String(teamOneGoal) // 팀 1 득점
+         teamTwoScore.text = String(teamTwoGoal) // 팀 2 득점
+         
+         let view_width = view.bounds.size.width // 현재 기기의 width 알아내기
+         
+         VictoryRateBar1_constr.constant = (view_width + VictoryRateBar_trailing_constr.constant - VictoryRateBar_leading_constr.constant) * CGFloat(homewin) // 팀1 승리 확률 막대 길이 조정
+         VictoryRateBar2_constr.constant = (view_width + VictoryRateBar_trailing_constr.constant - VictoryRateBar_leading_constr.constant) * CGFloat(draw) // 무승보 확률 막대 길이 조정
+         VictoryRateBar3_constr.constant = (view_width + VictoryRateBar_trailing_constr.constant - VictoryRateBar_leading_constr.constant) * CGFloat(awaywin) // 팀2 승리 확률 막대 길이 조정
+         
+         // 막대 크기 설정
+         VictoryRateBar1.setProgress(1, animated: false)
+         VictoryRateBar1.transform = VictoryRateBar1.transform.scaledBy(x: 1, y: 8)
+         VictoryRateBar2.setProgress(1, animated: false)
+         VictoryRateBar2.transform = VictoryRateBar2.transform.scaledBy(x: 1, y: 8)
+         VictoryRateBar3.setProgress(1, animated: false)
+         VictoryRateBar3.transform = VictoryRateBar3.transform.scaledBy(x: 1, y: 8)
+         
+         // 확률 텍스트로 출력
+         teamOneWinRate.text = "홈 승리: " + Int(homewin * 100).description + "%" // 팀1 승리 확률
+         teamTwoWinRate.text = "어웨이 승리: " + Int(awaywin * 100).description + "%" // 무승부 확률
+         DrawRate.text = "무승부: " + Int(draw * 100).description + "%" // 팀2 승리 확률
+         
         
-        let prediction = try? mlModel.prediction(input: MyTabularClassifier_1Input(home_team: teamOne, away_team: teamTwo))
-        let homePrediction = try? homeScoreModel.prediction(input: HomeScore_1Input(home_team: teamOne, away_team: teamTwo))
+        let prediction = try? mlModel.prediction(input: ClassificationInput(home_team: teamOne, away_team: teamTwo))
+        let homePrediction = try? homeScoreModel.prediction(input: HomeScoreInput(home_team: teamOne, away_team: teamTwo))
+        let awayPrediction =   try? awayScoreModel.prediction(input: HomeScore_3Input(home_team: teamOne, away_team: teamTwo))
         
         print(prediction!.result)
         print(prediction!.resultProbability)
         print(homePrediction!.home_goals)
-        
         homeScore = Int(round(homePrediction!.home_goals))
+        awayScore = Int(round(awayPrediction!.away_goals))
+        
+        teamOneScore.text = String(homeScore) // 팀 1 득점
+        teamTwoScore.text = String(awayScore) // 팀 2 득점
         resultDict = prediction!.resultProbability
         print(homeScore)
         for (key,value) in resultDict{
             if(key == "H"){
                 temp = Int(value * 100)
-                print("홈 팀이 이길확률 \(temp)%")
+                teamOneWinRate.text = "홈 승리: " + Int(value * 100).description + "%" // 팀1 승리 확률
+//                print("홈 팀이 이길확률 \(temp)%")
             }else if(key == "A"){
                 temp = Int(value * 100)
-                print("어웨이 팀이 이길확률 \(temp)%")
+                teamTwoWinRate.text = "어웨이 승리: " + Int(value * 100).description + "%" // 무승부 확률
+//                print("어웨이 팀이 이길확률 \(temp)%")
             }else{
                 temp = Int(value * 100)
-                print("경기가 무승부일 확률 \(temp)%")
+                DrawRate.text = "무승부: " + Int(value * 100).description + "%" // 팀2 승리 확률
+
+//                print("경기가 무승부일 확률 \(temp)%")
             }
         }
 
